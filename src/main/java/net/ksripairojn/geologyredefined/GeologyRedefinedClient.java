3a6488a4ccb575720cb488e5f945aed401765374
kattapis64 <kittiphasa20@gmail.com>
2025-11-26 11:30:06 +0700

    main

---
+++ src/main/java/net/ksripairojn/geologyredefined/GeologyRedefinedClient.java
@@ -0,0 +1,10 @@
+package net.ksripairojn.geologyredefined;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GeologyRedefinedClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
